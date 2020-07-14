df0f78451c65154d0394991f73d4611950cd4cfa
udasitharani <udasitharani@gmail.com>
2020-07-14 21:11:22 +0530

    add animations

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <app-navbar></app-navbar>
     <div class="routing-view">
-      <router-view></router-view>
+      <transition name="slide" appear mode="out-in">
+        <router-view></router-view>
+      </transition>
     </div>
   </div>
 </template>
@@ -23,5 +25,39 @@ export default {
 }
 .routing-view {
   margin: 20px 0;
+}
+
+.slide-enter {
+}
+.slide-enter-active {
+  animation: slide-in 0.25s ease-out forwards;
+}
+
+.slide-leave {
+}
+.slide-leave-active {
+  animation: slide-out 0.1s ease-out;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(30px);
+    opacity: 0.3;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(30px);
+    opacity: 0.3;
+  }
 }
 </style>
