3cc2aba9c67c644411679a35e821b47cdf96aaf8
udasitharani <udasitharani@gmail.com>
2020-07-14 13:35:03 +0530

    complete static navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <app-navbar></app-navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from "./components/Navigation";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    "app-navbar": Navigation
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  padding-top: 30px;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Stock Trader</a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Stocks</a>
+        </li>
+      </ul>
+    </div>
+    <div class="my-2 my-lg-0" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <span class="navbar-text">End Day</span>
+        </li>
+        <li class="nav-item">
+          <span class="navbar-text">Funds: ${{ fund }}</span>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  computed: {
+    fund() {
+      return "10.00";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.navbar {
+  border: 1px solid #dddddd;
+  border-radius: 2px;
+}
+.navbar-brand {
+  color: #666666;
+  font-weight: 500;
+}
+.nav-item {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+.nav-item .item {
+  color: #00000077;
+  /* padding: 0.5rem 1rem 0; */
+  /* font-size: 1rem; */
+}
+</style>
